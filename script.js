60fe066d82a81937287803057382f283673f9189
mateusbentes <mateusbentes@gmail.com>
2024-06-13 11:46:51 -0300

    edicao de comentarios

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// para poder adicionar nota na tabela
 document.getElementById('notaForm').addEventListener('submit', adicaoNota);
 
+// função para obter todas as notas do banco de dados pelo front-end
 function obterNotas() {
     fetch('http://127.0.0.1:5000/')
         .then(response => response.json())
@@ -7,24 +9,28 @@ function obterNotas() {
             const listaNotas = document.getElementById('listaNotas');
             listaNotas.innerHTML = '';
             data.Notas.forEach(nota => {
+                // mudança da tabela de acordo com com a formatação tr
                 const row = document.createElement('tr');
-                row.setAttribute('data-id', nota.id);
+                row.setAttribute('data-id', nota.id); // leitura do id de acordo com o data-id
+                // modificação do titulo, texto, botão de edição e botão de remoção na tabela
                 row.innerHTML = `
                     <td contenteditable="true">${nota.titulo}</td>
                     <td contenteditable="true">${nota.texto}</td>
                     <td><button onclick="edicaoNota(${nota.id})">Salvar</button></td>
                     <td><button onclick="remocaoNota(${nota.id})">Deletar</button></td>
                 `;
+                // adição do titulo, texto, botão de edição e botão de remoção na tabela
                 listaNotas.appendChild(row);
             });
         })
         .catch(error => console.error('Erro:', error));
 }
 
+// função para adicionar uma nota do banco de dados pelo front-end
 function adicaoNota(event) {
     event.preventDefault();
-    const titulo = document.getElementById('titulo').value;
-    const texto = document.getElementById('texto').value;
+    const titulo = document.getElementById('titulo').value; // adição do titulo
+    const texto = document.getElementById('texto').value; // adição do texto
 
     fetch('http://127.0.0.1:5000/', {
         method: 'POST',
@@ -45,9 +51,12 @@ function adicaoNota(event) {
     .catch(error => console.error('Erro:', error));
 }
 
+// função para editar uma nota do banco de dados pelo front-end
 function edicaoNota(id) {
-    const row = document.querySelector(`tr[data-id="${id}"]`);
+    const row = document.querySelector(`tr[data-id="${id}"]`); // edição por id na tabela
+    // edição da primeira linha (titulo) na tabela
     const titulo = row.querySelector('td:nth-child(1)').innerText;
+    // edição da segunda linha (texto) na tabela
     const texto = row.querySelector('td:nth-child(2)').innerText;
         
     fetch(`http://127.0.0.1:5000/`, {
@@ -69,6 +78,7 @@ function edicaoNota(id) {
     .catch(error => console.error('Error:', error));
 }
 
+// função para remover uma nota do banco de dados pelo front-end
 function remocaoNota(id) {
     fetch(`http://127.0.0.1:5000/`, {
         method: 'DELETE',
